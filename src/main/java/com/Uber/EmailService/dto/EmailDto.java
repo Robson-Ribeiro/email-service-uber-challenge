ded0a92a05670b63968de8aa6ed9780c056b416f
Robson <robsonribeirorra@gmail.com>
2024-01-24 17:31:14 -0300

    EmailDto created

--- src/main/java/com/Uber/EmailService/dto/EmailDto.java
+++ src/main/java/com/Uber/EmailService/dto/EmailDto.java
@@ -1,5 +1,25 @@
 package com.Uber.EmailService.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class EmailDto {
     
+    private Long id;
+
+    @NotBlank
+    private String from;
+    @NotBlank
+    private String to;
+    @NotBlank
+    private String subject;
+    @NotBlank
+    private String body;
 }
