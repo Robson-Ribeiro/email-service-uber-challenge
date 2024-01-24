52eeb575978e1aabc03a7913c8e511a276fcd1b3
Robson <robsonribeirorra@gmail.com>
2024-01-24 17:41:36 -0300

    EmailController created

---
+++ src/main/java/com/Uber/EmailService/controller/EmailController.java
@@ -0,0 +1,27 @@
+package com.Uber.EmailService.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Uber.EmailService.dto.EmailDto;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping(value = "/email")
+@CrossOrigin
+public class EmailController {
+    
+    //@Autowire
+    //private EmailService emailService;
+
+    @PostMapping
+    public ResponseEntity sendEmail(@Valid @RequestBody EmailDto email) {
+        //emailService.sendEmail(email);
+        return ResponseEntity.ok().body("Your email has been sent successfully!");
+    }
+}
