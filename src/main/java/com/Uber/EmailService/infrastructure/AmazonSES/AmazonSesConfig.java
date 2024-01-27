ca417e26d73c2bd5b94cd0a04b7ba4c86eb37190
Robson <robsonribeirorra@gmail.com>
2024-01-27 00:42:53 -0300

    Configuring the SesClient instance and debugging it

--- src/main/java/com/Uber/EmailService/infrastructure/AmazonSES/AmazonSesConfig.java
+++ src/main/java/com/Uber/EmailService/infrastructure/AmazonSES/AmazonSesConfig.java
@@ -1,17 +1,32 @@
 package com.Uber.EmailService.infrastructure.AmazonSES;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
 
 @Configuration
 public class AmazonSesConfig {
+
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    @Value("${aws.accessKeyId}")
+    private String accessKey;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
     
     @Bean
     public AmazonSimpleEmailService amazonSimpleEmailService() {
-        return AmazonSimpleEmailServiceClientBuilder.standard().build();
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        return AmazonSimpleEmailServiceClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(Regions.US_EAST_2).build();
     }
 
 }
